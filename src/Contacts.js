8bdec7cc3ef11ab3ffe5fc4cb36b50cc20d8b1c6
adam iqshan <omai.mohmed9@gmail.com>
2020-12-28 21:33:45 +0530

    contacts component added

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import "./Contacts.css"
+import { Avatar } from "@material-ui/core"
+import db from "./firebase"
 
 
 function Contacts() {
+
+    const [contacts, setContacts] = useState([])
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+            setContacts(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        );
+        console.log(contacts)
+    }, []);
+
     return (
         <div className="contacts">
             <div className="contacts__top">
@@ -16,8 +31,15 @@ function Contacts() {
                     <MoreHorizIcon />
                 </div>
             </div>
+            {contacts.map((contact) => (
+                <div className="contacts__list">
+                    <Avatar src={contact.data.profilePic} />
+                    <h4>{contact.data.username}</h4>
+                </div>
 
+            ))}
         </div>
+
     )
 }
 
